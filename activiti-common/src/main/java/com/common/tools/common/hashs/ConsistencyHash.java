b6bc14414133f1bfc66153da890c0ea9576f37fb
jingyan <jingyan@le.com>
2017-04-19 17:05:57 +0800

    修改结构

--- activiti-common/src/main/java/com/common/tools/common/hashs/ConsistencyHash.java
+++ activiti-common/src/main/java/com/common/tools/common/hashs/ConsistencyHash.java
@@ -118,3 +118,42 @@ public class ConsistencyHash {
     }
 }
 
+/**
+ * @Author: jingyan
+ * @Time: 2017/4/19 17:05
+ * @Describe:服务器信息实体类
+ */
+class ServerNodeInfo {
+
+    private String name;
+    private String ip;
+
+    public ServerNodeInfo(String name, String ip) {
+        this.name = name;
+        this.ip = ip;
+    }
+
+    @Override
+    public String toString() {
+        return "ServerNodeInfo{" +
+                "name='" + name + '\'' +
+                ", ip='" + ip + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
